7281852470b530809b85f871a5c01725d077f7d5
blankhall98 <blankhall@ciencias.unam.mx>
2023-05-23 19:45:14 -0600

    initial structure

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
+#imports
 from flask import Flask
+from flask_restful import Api, Resource, reqparse, abort
 
 #initialize app
 app = Flask(__name__)
+
+########## routes
+
+#index
+@app.route('/')
+def index():
+    return '<h1>sandb</h1>' 
+
+##########
 
 #run app
 if __name__ == '__main__':
